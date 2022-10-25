bf482f2a15819c2bdf88b7bdc77c97ce83e53e14
amirbahador <amirbahador.pv@gmail.com>
2022-10-25 18:08:07 -0400

    :card_file_box: Add blog models

--- embed/blog/models.py
+++ embed/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from embed.common.models import BaseModel
+from embed.users.models  import BaseUser
 
-# Create your models here.
+
+class Post(BaseModel):
+
+    slug = models.SlugField(
+            primary_key=True,
+            max_length=100,
+    )
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+    content = models.CharField(
+        max_length=1000,
+    )
+
+    def __str__(self):
+        return self.title
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name="subs")
+    target     = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name="targets")
+
--- embed/users/models.py
+++ embed/users/models.py
@@ -1,28 +1,23 @@
-import uuid
-
 from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager as BUM,
     PermissionsMixin,
     AbstractBaseUser
 )
-
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from embed.common.models import BaseModel
-
-
-# Taken from here:
-# https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#a-full-example
-# With some modifications
+from django.utils.translation import gettext_lazy as _
 
 
 class BaseUserManager(BUM):
-    def create_user(self, email, is_active=True, is_admin=False, password=None):
+    def create_user(self, username, email, is_active=True, is_admin=False, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
         user = self.model(
             email=self.normalize_email(email.lower()),
             is_active=is_active,
+            username=username,
             is_admin=is_admin
         )
 
@@ -36,12 +31,13 @@ class BaseUserManager(BUM):
 
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, username, email, password=None):
         user = self.create_user(
             email=email,
             is_active=True,
             is_admin=True,
             password=password,
+            username=username,
         )
 
         user.is_superuser = True
@@ -51,6 +47,22 @@ class BaseUserManager(BUM):
 
 
 class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
@@ -62,10 +74,28 @@ class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
 
     objects = BaseUserManager()
 
-    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
 
     def __str__(self):
         return self.email
 
     def is_staff(self):
         return self.is_admin
+
+
+class Profile(models.Model):
+    user                = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
+    posts_count         = models.PositiveIntegerField(default=0)
+    subscriptions_count = models.PositiveIntegerField(default=0)
+    subscribers_count   = models.PositiveIntegerField(default=0)
+    bio                 = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return f"{self.user.username} >>> {self.bio}"
+
