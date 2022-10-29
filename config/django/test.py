707797671c61688204731015326835338f93a776
amirbahador <amirbahador.pv@gmail.com>
2022-10-29 15:04:13 -0400

    :bug: Add sqlite for test

--- config/django/test.py
+++ config/django/test.py
@@ -18,6 +18,6 @@ CACHES = {
 DATABASES = {
         "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR + "db.sqlite3",
         }
     }
