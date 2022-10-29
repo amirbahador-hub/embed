ab8fd75b8e5cbf9f4fb0c8be8d9887abac2b7505
amirbahador <amirbahador.pv@gmail.com>
2022-10-29 18:53:56 -0400

    :test_tube: Add Test subs api

--- embed/tests/apis/test_auth.py
+++ embed/tests/apis/test_auth.py
@@ -10,38 +10,37 @@ import json
 def test_unauth_post_api(user1, subscription1, profile1, post1):
     client = Client()
     url_ = reverse("api:blog:post")
-            
-    response = client.post(url_, content_type='application/json')
+
+    response = client.post(url_, content_type="application/json")
 
     assert response.status_code == 401
+
 
 @pytest.mark.django_db
 def test_auth_api(api_client, user1, subscription1, profile1, post1):
     url_ = reverse("api:blog:post")
-            
-    response = api_client.get(url_, content_type='application/json')
 
-    assert response.status_code 200
+    response = api_client.get(url_, content_type="application/json")
+
+    assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_login(user1, subscription1, profile1, post1):
-    user = BaseUser.objects.create_user(username='john', email='js@js.com', password='js.sj')
+    user = BaseUser.objects.create_user(
+        username="john", email="js@js.com", password="js.sj"
+    )
 
     client = APIClient()
     url_ = reverse("api:authentication:jwt:login")
-    body = {
-            "username":user.username,
-            "password":"js.sj"
-            }
-    response = client.post(url_, json.dumps(body), content_type='application/json')
+    body = {"username": user.username, "password": "js.sj"}
+    response = client.post(url_, json.dumps(body), content_type="application/json")
     auth = json.loads(response.content)
     access = auth.get("access")
     refresh = auth.get("refresh")
 
     assert access != None
     assert type(access) == str
-    
+
     assert refresh != None
     assert type(refresh) == str
- 
